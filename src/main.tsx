f2f9edc2c15a13b0611fa6ac4cc2dd62875de0f5
Shah Alam <shahalam01660166344@gmail.com>
2024-08-30 21:22:08 +0600

    add redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.tsx'
 import { Toaster } from 'sonner'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Toaster position="top-center"/>
-    <RouterProvider router={router} />
+    <Toaster position="top-center" />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./api/baseApi";
+import productReducer from "./feature/cartSlice";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "cart",
+  storage,
+};
+
+const productCartReducer = persistReducer(persistConfig, productReducer);
 
 export const store = configureStore({
-  reducer: {},
-})
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+    product: productCartReducer,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
